965970389eb08efaa45191e1a6ee9d3822e08139
patarebenezer <patar@kulego.id>
2024-05-02 12:25:29 +0700

    fix(landing-page): added suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import LandingPage from "./modules/LandingPage";
 import Provider from "@/app/utils/provider";
 
 export default function Home() {
  return (
   <Provider>
-   <LandingPage />
+   <Suspense fallback='Loading..'>
+    <LandingPage />
+   </Suspense>
   </Provider>
  );
 }
